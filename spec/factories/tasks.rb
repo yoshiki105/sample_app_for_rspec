a1b78786249909ec31da0d52182c5dbeac84c1a3
Yoshiki Hasumi <yoshiki.h.105@gmail.com>
2021-03-30 10:00:24 +0900

    [Add]user、taskのテストデータを作成。Taskモデルのバリデーションテストを追加。

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :task do
-    
+    sequence(:title) { |n| "Test task #{n}" }
+    content { 'Sample content.' }
+    status { :todo }
+    deadline { 1.week.from_now }
+    association :user
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    it 'is valid with all attributes' do
+      task = FactoryBot.build(:task)
+      expect(task).to be_valid
+    end
+    it 'is invalid without title' do
+      task = FactoryBot.build(:task, title: '')
+      task.valid?
+      expect(task.errors).to_not be_empty
+      expect(task.errors[:title]).to eq ["can't be blank"]
+    end
+    it 'is invalid without status' do
+      task = FactoryBot.build(:task, status: nil)
+      task.valid?
+      expect(task.errors).to_not be_empty
+      expect(task.errors[:status]).to eq ["can't be blank"]
+    end
+    it 'is invalid with a duplicate title' do
+      task = FactoryBot.create(:task, title: 'Sample title')
+      other_task = FactoryBot.build(:task, title: 'Sample title')
+      other_task.valid?
+      expect(other_task.errors).to_not be_empty
+      expect(other_task.errors[:title]).to eq ['has already been taken']
+    end
+    it 'is valid with another title' do
+      task = FactoryBot.create(:task, title: 'Sample title')
+      other_task = FactoryBot.build(:task, title: 'Another title')
+      other_task.valid?
+      expect(other_task).to be_valid
+    end
+  end
 end
